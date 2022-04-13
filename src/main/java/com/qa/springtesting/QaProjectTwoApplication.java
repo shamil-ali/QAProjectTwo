ddabf05e6dca22fa26ec8c35428c9a93ec401078
shamil-ali <shamilsali@aol.co.uk>
2022-04-13 16:07:25 +0100

    first commit

---
+++ src/main/java/com/qa/springtesting/QaProjectTwoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaProjectTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaProjectTwoApplication.class, args);
+	}
+
+}
